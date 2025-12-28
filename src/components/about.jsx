bee942415e7b6138ec50f1fbb2dd6faea366a8c4
Ranjit Rautaray <ranjit.r@terralogic.com>
2025-12-28 12:10:18 +0530

    some changes done in about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About testing  </div>
+        <div>About testing
+            <p>testing again</p>
+        </div>
     )
 }
 
