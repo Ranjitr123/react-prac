62775740fa4bf9df509e6a0cc67fdddfcc12957e
Ranjit Rautaray <ranjit.r@terralogic.com>
2026-01-04 19:18:17 +0530

    fixed index.js

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-dotenv.config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
